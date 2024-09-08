65772988bca0c56eb6d99ee147361b1fd431b8eb
Ragib Shariar <srragib@gmail.com>
2024-09-08 19:01:02 +0600

    fix base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { RootState } from "../store";
 const baseQuery = fetchBaseQuery({
   // baseUrl: "https://sports-facilit-booking-platform.vercel.app/api",
   // baseUrl: "http://localhost:5000/api",
-  baseUrl: "https://googleapi.labontest.tech",
+  baseUrl: "https://googleapi.labontest.tech/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
